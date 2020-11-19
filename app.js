899c87fa5cd6436fe59331076dcdd61971cf5c63
burkehollandmsft <64420201+burkehollandmsft@users.noreply.github.com>
2020-11-19 20:42:45 +0000

    add about template

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
